8f5fbab413833ef895dd879e88f50eed8090806e
Kevin <ktorres93@knights.ucf.edu>
2017-09-14 21:06:28 -0400

    test on new pc

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
       <div class="row">
         <div class="col-sm-8">
         <h2> About Me</h2> <hr size="558" style="background-color:#d3d3d3;"> 
-        <p> <img style= "float:left;" src= "images/profile.jpg" class="profile" >ipsumsdhashd sdhl ashd lakshd ahsd jahd haskjd hakj dhakj hajk hakjsh dajkh djashd jahdj ahskjahsj hajk dhasjk dhaskjd hkaj shdjas dhja hjas hjaksh djkas dhasj dhasjd hsajd khas djhasdasb ah fjfdhb hkjfkhksjfhsdjksjdhskjdf jkahsdfhjdghjfgkjsdgfasdfhgsakjfgsdfgkksajfgkkfjhg fkjashdfgkfsagfsaf<p>
+        <p> <img style= "float:left;" src= "images/profile.jpg" class="profile" >ipsumsdhashd sdhl ashd lakshd ahsd jahd haskjd hakj dhakj hajk hakjsh dajkh djashd jahdj ahskjahsj hajk dhasjk dhaskjd hkaj shdjas dhja hjas hjaksh djkaasd;kasds dhasj dhasjd hsajd khas djhasdasb ah fjfdhb hkjfkhksjfhsdjksjdhskjdf jkahsdfhjdghjfgkjsdgfasdfhgsakjfgsdfgkksajfgkkfjhg fkjashdfgkfsagfsaf<p>
           <p>gsdjfgsdjh kakjf sidfjdhfkjsdhfkjsdhfjkshflksjdhfkjsdkhlfkjsdhfjshf sdfhksdj fhskjd fhksfjkhsdlkf shjfk sfjhsfdhjksldhfkjshasdfkhlsjfhafkjhaljkf lasf hasjldflhjsdkhfjsahflsdhhksdjhflksjdhfjsdfhakdfhjhaskjljhjhjkhd hakjd hakjs dhakj hjk dhakj haj kdhasjk dhas jkhjkas hsajk hasjkd hasjkdh jaksdh jks haj dhjksd hajks hajksd hajksd hasjkd hhhhhhhhhhhakdjh akjdh jaskdh ajskdh jakshd <p></div>
         <div class="col-sm-4">
 
